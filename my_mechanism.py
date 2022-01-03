43b3c605d6b09b28f262d08abe3def80b1320549
Sergio Gimenez <sergiogimenezanton@gmail.com>
2022-01-03 18:29:03 +0100

    Hello world driver is working now

--- my_mechanism.py
+++ my_mechanism.py
@@ -1,9 +1,11 @@
-from neutron.openstack.common import log as logging
-from neutron.plugins.ml2 import driver_api as api
-
+from oslo_log import log as logger
+from neutron_lib.plugins.ml2 import api
 LOG = logging.getLogger(__name__)
+
+LOG = logger.getLogger(__name__)
+
 
 class MyDriver(api.MechanismDriver):
 
-   def initialize(self):
-        LOG.info("Hello, mydriver initialize)
+    def initialize(self):
+        LOG.info("Hello, mydriver initialize")
