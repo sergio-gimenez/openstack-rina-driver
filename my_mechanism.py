c3053fabdb1a2992d6e73bad5b1ba3eb467c52d0
Sergio Gimenez <sergiogimenezanton@gmail.com>
2022-01-13 10:27:52 +0100

    [WIP] Trying to get net segment info. Fixing import

--- my_mechanism.py
+++ my_mechanism.py
@@ -3,7 +3,7 @@ from oslo_log import log as logger
 # Import Neutron Database API
 from neutron_lib.plugins.ml2 import api
 # Import ML2 Database API
-from neutron_lib.plugins.ml2 import db as ml2_db
+from neutron_lib import db as ml2_db
 
 LOG = logger.getLogger(__name__)
 
